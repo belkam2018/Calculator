d73e0ab727a4df5df5a60783252b36e5e8df4d33
belkam2018 <46766577+belkam2018@users.noreply.github.com>
2019-01-19 13:03:45 -0500

    Add multiply method

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,4 +11,7 @@ public class Calculator {
     public static double div(double num1,double num2){
         return num1 / num2;
     }
+    public static double multiplay(double num1, double num2){
+        return num1 * num2;
+    }
 }
