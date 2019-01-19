644b89b2a9183060b6b739b3c2c4fac0712416f9
kamal belkati <belkam2004@yahoo.fr>
2019-01-19 16:49:18 -0500

    add main method

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,19 +1,27 @@
 public class Calculator {
     // this is a very usful method
-    public  static double add(double num1, double num2){
+    public static double add(double num1, double num2) {
         return num1 + num2;
     }
 
-    public static double substract(double num1, double num2){
+    public static double substract(double num1, double num2) {
         double result = num1 - num2;
         return result;
     }
-    public static double div(double num1,double num2){
+
+    public static double div(double num1, double num2) {
         return num1 / num2;
     }
-    public static double multiply(double num1, double num2){
+
+    public static double multiply(double num1, double num2) {
         return num1 * num2;
     }
-     public static double rem(double num1,double num2){
+
+    public static double rem(double num1, double num2) {
         return num1 % num2;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
