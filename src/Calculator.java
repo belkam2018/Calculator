c34527aeb99bcf8964f64ccc58ea21bba6a5d4bb
belkam2018 <46766577+belkam2018@users.noreply.github.com>
2019-01-19 17:13:36 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -17,7 +17,7 @@ public class Calculator {
     public static double multiply(double num1, double num2) {
         return num1 * num2;
     }
-
+ // rem method
     public static double rem(double num1, double num2) {
         return num1 % num2;
     }
